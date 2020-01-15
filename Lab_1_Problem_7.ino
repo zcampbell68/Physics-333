dc730133da75e0ce83a53fa18676ab5bf469fd56
Zachary Campbell <59896718+zcampbell68@users.noreply.github.com>
2020-01-15 11:59:20 -0600

    Add files via upload

---
+++ Lab_1_Problem_7.ino
@@ -0,0 +1,44 @@
+void setup() {
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  int Fast = 500;
+  int Slow = 1000;
+  digitalWrite(13, HIGH);
+  delay(Slow);
+ digitalWrite(13, LOW);
+  delay(Slow);
+   digitalWrite(13, HIGH);
+  delay(Slow);
+ digitalWrite(13, LOW);
+  delay(Slow);
+   digitalWrite(13, HIGH);
+  delay(Slow);
+ digitalWrite(13, LOW);
+  delay(Slow);
+  digitalWrite(13, HIGH);
+  delay(Fast);
+ digitalWrite(13, LOW);
+  delay(Fast);
+  digitalWrite(13, HIGH);
+  delay(Fast);
+ digitalWrite(13, LOW);
+  delay(Fast);
+  digitalWrite(13, HIGH);
+  delay(Fast);
+ digitalWrite(13, LOW);
+  delay(Fast);
+  digitalWrite(13, HIGH);
+  delay(Slow);
+ digitalWrite(13, LOW);
+  delay(Slow);
+   digitalWrite(13, HIGH);
+  delay(Slow);
+ digitalWrite(13, LOW);
+  delay(Slow);
+   digitalWrite(13, HIGH);
+  delay(Slow);
+ digitalWrite(13, LOW);
+  delay(Slow);
+}
